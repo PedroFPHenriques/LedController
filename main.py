f721fb1f0330d2303f2289c1e06163435b124d85
Pedro <pedrofphenriques@hotmail.com>
2021-11-27 19:11:26 -0100

    version 0.5

--- main.py
+++ main.py
@@ -17,16 +17,11 @@ def map_color(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def setColor(col):
-    R_val = col >> 8
+    B_val = col >> 8
     G_val = col & 0x00FF
-    print(R_val)
-    print(G_val)
 
-    R_val = map_color(R_val, 0, 255, 0, 100)
+    R_val = map_color(B_val, 0, 255, 0, 100)
     G_val = map_color(G_val, 0, 255, 0, 100)
-
-    print(R_val)
-    print(G_val)
 
     p_R.ChangeDutyCycle(R_val)
     p_G.ChangeDutyCycle(G_val)
@@ -39,7 +34,7 @@ def hello():
 
 @app.route('/on')
 def on():
-    p_R.start(0)
+    p_B.start(0)
     p_G.start(0)
     setColor(0x00FF00)
     return render_template('on.html')
@@ -47,7 +42,7 @@ def on():
 
 @app.route('/off')
 def off():
-    p_R.stop()
+    p_B.stop()
     p_G.stop()
     GPIO.output(pins, GPIO.LOW)
     return render_template('off.html')
